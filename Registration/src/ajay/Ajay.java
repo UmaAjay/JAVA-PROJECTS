57160e21f663201597e537af106f6b7d2a1f2364
UMA AJAY KUMAR REDDY P S <89115880+UmaAjay@users.noreply.github.com>
2022-06-01 21:29:03 +0530

    Add files via upload

---
+++ Registration/src/ajay/Ajay.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ajay;
+
+/**
+ *
+ * @author umave
+ */
+public class Ajay {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String str="ajay";
+        StringBuilder str1=new StringBuilder();
+        str1.append(str);
+        str1=str1.reverse();
+        System.out.println(str1);
+    }
+    
+}
